a329d928125bbde1bfbfc780f07bf6789c0bfdb7
Макс <r2d2@r2d2s-Mac-mini.local>
2022-04-30 09:48:09 +0300

    delete employers

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -5,23 +7,39 @@ import EmployerList from '../employers-list/employers-list'
 import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
-function App() {
-    const data = [
-        {name: 'Max.F', salary: 8000, increase: false, id: 1},
-        {name: 'Alex.A', salary: 600, increase: true, id: 2},
-        {name: 'John.S', salary: 400, increase: false, id: 3}
-    ]
-    return(
-        <div className="app">
-            <AppInfo/>
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            data: [
+                {name: 'Max.F', salary: 8000, increase: false, id: 1},
+                {name: 'Alex.A', salary: 600, increase: true, id: 2},
+                {name: 'John.S', salary: 400, increase: false, id: 3}
+            ]
+        }
+    }
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return{
+                data: data.filter(item =>  item.id !== id)
+            }
+        })
+    }
+    
+    render() {
+        return(
+            <div className="app">
+                <AppInfo/>
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                <EmployerList data={this.state.data} onDelete={this.deleteItem}/>
+                <EmployersAddForm/>
             </div>
-            <EmployerList data={data}/>
-            <EmployersAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
