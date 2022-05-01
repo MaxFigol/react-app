c5c51153a1c45213a0571b21f825e69120fafae4
Макс <r2d2@r2d2s-Mac-mini.local>
2022-05-01 21:26:13 +0300

    add filter

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,12 +1,26 @@
 import './app-filter.css'
 
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+    const btnData = [
+        {name: 'all', label: 'Все сотрудники'},
+        {name: 'rise', label: 'На повышение'},
+        {name: '1000', label: 'З/П 1000$'},
+    ]
+
+    const buttons = btnData.map(({name, label}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-primary' : 'btn-info'
+        return(
+            <button className={`btn ${clazz}`} type="button" key={name} onClick={() => props.onFilterSelect(name)}>{label}</button>
+        )
+    })
     return(
         <div className="btn-group">
-            <button className="btn btn-primary" type="button">Все сотрудники</button>
+            {buttons}
+            {/* <button className="btn btn-primary" type="button">Все сотрудники</button>
             <button className="btn btn-info" type="button">На повышение</button>
-            <button className="btn btn-info" type="button">З/П 1000$</button>
+            <button className="btn btn-info" type="button">З/П 1000$</button> */}
         </div>
     )
 }
